5d857281b2bbe6bb93649c7a9ff3f606df6842e9
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2025-09-10 18:38:36 +0530

    fixing missing data

---
+++ Machine_Learning_A-Z/missing_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Data.csv")
+
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+print(X)
+print(y)
+
+# handling missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean') # missing_values = np.nan is optional as it is default and strategy = 'mean' is also default
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
