4500d1eb806ce1fd2eaaaf49c3eb18620057dbf7
Abhay-Pratap-Singh-7 <abhaypratapsingh2993@gmail.com>
2025-09-11 21:10:14 +0530

    Linear Regression

---
+++ Regression/simple_regression.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("/Users/abhay/Desktop/Machine Learning/Regression/Salary_Data.csv")
+
+X = dataset.iloc[:, 0:1].values
+y = dataset.iloc[:, 1:].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Exprience')
+plt.xlabel('Salary')
+plt.ylabel('Exprience')
+plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Exprience')
+plt.xlabel('Salary')
+plt.ylabel('Exprience')
+plt.show()
--- plotting.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv("/Users/abhay/Desktop/Machine Learning/Dataset/housing.csv")
-
-data.plot(kind = 'scatter', x = "median_income" , y = "median_house_value" , figsize=(8,8) , alpha = 0.1)
-plt.show()
-
